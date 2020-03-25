b82ae0bacaf964e360ad2b33be94b45a1b2dcef3
atal1 <pandey.atal@outlook.com>
2020-03-25 19:54:23 +0530

    Removed extra annotaiton

--- json_project/src/main/java/practise/Order.java
+++ json_project/src/main/java/practise/Order.java
@@ -4,12 +4,9 @@ package practise;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "StandardHeader", "Body" })
+
 public class Order {
 
 	@JsonProperty("StandardHeader")
